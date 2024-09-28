6284d046e68d445fe556c2b33dd44c2882d3ff48
Glenn <glcheruv77@gmail.com>
2024-09-27 21:03:21 -0700

    Task 1: Checking number of grades

---
+++ Grader.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+// import java.util.Arrays;
+
+public class Grader {
+    public static void main(String[] args){
+        //Task 1: Checking if the # of grade is positive
+        Scanner Input = new Scanner(System.in);
+        System.out.println("Enter number of grades");
+        int numGrades  = Input.nextInt();
+        
+        while ( numGrades < 0 && numGrades > 100){
+            System.out.println("Please enter a positive number");
+            numGrades = Input.nextInt();
+        }
+
+        //Creating Array to store grades
+        // System.out.println("Please enter the grades separated by spaces");
+        // int[] listOfGrades = Input.nextInt();
+
+        // for(int i = 0; i < numGrades; i++){
+        //     System.out.println(listOfGrades[i]);
+        // }
+
+
+        
+
+        Input.close();
+    }
+    
+}
