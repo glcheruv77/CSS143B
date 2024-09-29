5521a385266fdd12a9564bd8ad1067f24f0bb161
Glenn <glcheruv77@gmail.com>
2024-09-29 12:47:59 -0700

    Task 6: Complilying All the methods into a file.

--- Grader.java
+++ Grader.java
@@ -1,28 +1,47 @@
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Grader {
     public static void main(String[] args){
         //Task 1: Checking if the # of grade is positive
         Scanner Input = new Scanner(System.in);
-        System.out.println("Enter number of grades");
+        System.out.println("Enter number of grades.");
         int numGrades  = Input.nextInt();
         
         while (numGrades < 0){
-            System.out.println("Please enter a positive number");
+            System.out.println("Please enter a positive number.");
             numGrades = Input.nextInt();
         }
 
         //Task 2: Creating Array to store grades
-        System.out.println("Please enter the grades separated by spaces");
+        System.out.println("Please enter the grades separated by spaces.");
         int[] IntGrades = new int[numGrades];
         for (int i = 0; i < numGrades; i++){
             IntGrades[i] = Input.nextInt();
         }
 
         for (int i = 0; i < IntGrades.length; i++){
-            System.out.println("Grade " + i + ":"+ IntGrades[i]);
+            System.out.println(IntGrades[i]);
         }
-    
+
+        //Task 6: Wrting everything into a file
+        System.out.println("PLease provide a file name to store your grades.");
+        String Filename = Input.next();
+        try {
+            FileWriter myFile = new FileWriter(Filename +".txt");
+            double[] doubleArray = new double[IntGrades.length];
+            for (int l = 0; l < IntGrades.length; l++){
+                myFile.write(IntGrades[l] + " " + getLetterGrade(IntGrades[l]) + "\n");
+                doubleArray[l] = (double) IntGrades[l];
+            }
+            double grade = average(doubleArray);
+            myFile.write("Average: " + grade + " " + getLetterGrade(grade));
+            
+            myFile.close();
+         } catch (IOException exception) {
+            System.out.println(exception.toString());
+         } 
 
         Input.close();
     }
@@ -33,14 +52,19 @@ public class Grader {
         switch (grade / 10) {
             case 10:
                 letterGrade = "A";
+                break;
             case 9:
                 letterGrade = "A";
+                break;
             case 8:
                 letterGrade = "B";
+                break;
             case 7:
                 letterGrade = "C";
+                break;
             case 6:
                 letterGrade = "D";
+                break;
             default:
                 letterGrade = "F";
         }
@@ -61,13 +85,13 @@ public class Grader {
     //Task 5: Same as Task 3 but using If instead of Switch.
     public static String getLetterGrade (double grade){
         String LetterGrade;
-        if (grade > 90) {
+        if (grade <= 100 && grade >= 90) {
             LetterGrade = "A";
-        } else if (grade > 80){
+        } else if ( grade < 90 && grade >= 80) {
             LetterGrade = "B";
-        } else if( grade > 70){
+        } else if ( grade < 80 && grade >= 70) {
             LetterGrade = "C";
-        } else if (grade > 60){
+        } else if (grade < 70 && grade >= 60) {
             LetterGrade = "D";
         } else {
             LetterGrade = "F";
